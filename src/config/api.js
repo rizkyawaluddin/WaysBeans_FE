36fa7838f1497d08a28bed2a8f25303dfec69d2d
rizkyawaluddin <rizkyawaluddin010498@gmail.com>
2022-09-16 17:05:44 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+// Create base URL API
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: process.env.REACT_APP_BASEURL, // Get REACT_APP_BASEURL from env here ...
 });
 
+// Set Authorization Token Header
 export const setAuthToken = (token) => {
   if (token) {
     API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   } else {
-    delete API.defaults.headers.commin["Authorization"];
+    delete API.defaults.headers.common["Authorization"];
   }
 };
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -115,8 +115,7 @@ export default function Cart() {
         //change this to the script source you want to load, for example this is snap.js sandbox env
         const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
         //change this according to your client-key
-        const myMidtransClientKey = "SB-Mid-client-E5TTsPupSznAjEa7";
-
+        const myMidtransClientKey = process.env.REACT_APP_MIDTRANS_CLIENT_KEY;
         let scriptTag = document.createElement("script");
         scriptTag.src = midtransScriptUrl;
         // optional if you want to set script attribute
